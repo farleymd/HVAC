d4e0879fb548f92f47d0730b749ebdc956cb50ab
farleymd <Butter15!>
2015-03-29 12:46:00 -0500

    GUI Form Layout

---
+++ ServiceGUI.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+/**
+ * Created by Marty on 3/29/2015.
+ */
+public class ServiceGUI {
+    private JCheckBox furnaceCheck;
+    private JCheckBox acCheck;
+    private JLabel addressLabel;
+    private JTextField addressText;
+    private JLabel descriptionLabel;
+    private JTextField descriptionText;
+    private JLabel reportedLabel;
+    private JTextField textField1;
+    private JLabel feeLabel;
+    private JTextField feeText;
+    private JLabel typeText;
+    private JComboBox comboBox1;
+}
