db811200b0fe169a1ba8ecde63d721e09baa2f2a
farleymd <Butter15!>
2015-03-29 13:35:01 -0500

    GUI Form Layout

--- ServiceGUI.java
+++ ServiceGUI.java
@@ -1,9 +1,10 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Marty on 3/29/2015.
  */
-public class ServiceGUI {
+public class ServiceGUI extends JFrame {
     private JCheckBox furnaceCheck;
     private JCheckBox acCheck;
     private JLabel addressLabel;
@@ -11,9 +12,42 @@ public class ServiceGUI {
     private JLabel descriptionLabel;
     private JTextField descriptionText;
     private JLabel reportedLabel;
-    private JTextField textField1;
+    private JTextField dateText;
     private JLabel feeLabel;
     private JTextField feeText;
     private JLabel typeText;
-    private JComboBox comboBox1;
+    private JComboBox typeBox;
+    private JList serviceCallList;
+    private JButton addTicketButton;
+    private JButton clearFieldsButton;
+    private JButton resolveTicketButton;
+    private JButton deleteTicketButton;
+    private JButton quitButton;
+    private JPanel rootPanel;
+
+    DefaultListModel<ServiceCall> serviceCallListModel;
+
+    final String FORCED_AIR = "Forced Air";
+    final String BOILER = "Boiler";
+    final String OCTOPUS = "Octopus";
+
+    public ServiceGUI(){
+        super("Service Call Manager");
+        setContentPane(rootPanel);
+        pack();
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        setSize(new Dimension(300,500));
+
+        serviceCallListModel = new DefaultListModel<ServiceCall>();
+        serviceCallList.setModel(serviceCallListModel);
+
+        serviceCallList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+        typeBox.addItem(FORCED_AIR);
+        typeBox.addItem(BOILER);
+        typeBox.addItem(OCTOPUS);
+    }
+
+
 }
